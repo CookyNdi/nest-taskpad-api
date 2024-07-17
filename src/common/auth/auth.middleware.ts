b4981a232cc21d70645a95c33744c4f6539682fa
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-07-17 12:05:57 +0700

    feat : adding else condition if user not found

--- src/common/auth/auth.middleware.ts
+++ src/common/auth/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -7,12 +7,15 @@ export class AuthMiddleware implements NestMiddleware {
   constructor(private prismaService: PrismaService) {}
   async use(req: any, res: any, next: () => void) {
     const token = req.headers['authorization'] as string;
+    console.log({ token });
     if (token) {
       const account = await this.prismaService.account.findFirst({
         where: { token: token },
       });
       if (account) {
         req.account = account;
+      } else {
+        throw new HttpException('Unauthorized!', 401);
       }
     }
     next();
